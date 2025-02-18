1b90161f6a7e44197f0b994de74101021486b7e6
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-02-17 21:39:31 -0300

    Respostas primeiro TBL -  parte individual

---
+++ Questao2.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+  string obj = "123";
+    int resultado = int.Parse(obj);
+    Console.WriteLine (resultado);
+  }
+}
---
+++ Questao3.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string numStr = "99";
+    int num = int.Parse(numStr);
+    Console.WriteLine (num);
+  }
+}
