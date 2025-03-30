da9f8f64235aa862ce3bb874188482fb63fc42fc
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-03-30 18:02:51 -0300

    Roteiro 1 exercicio 2
    Roteiro 1 exercicio 2 - Fantasma do PACMAN

---
+++ roteiro1-ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Fantasma {
+    public string Nick;
+    public string Cor;
+    public string Habilidade;
+    
+    public void GerarFantasma() {
+        Console.WriteLine($"Nome: {Nick} Cor: {Cor} Habilidade: {Habilidade}");
+    }
+
+    public void Mover(string direcao) {
+        Console.WriteLine($"O {Nick} se moveu para {direcao}");
+    }
+}
+
+public class Program {
+    public static void Main() {
+        Fantasma f1 = new Fantasma();
+        f1.Nick = "Ronaldo";
+        f1.Cor = "Vermelho";
+        f1.Habilidade = "Soltar lazer";
+        f1.GerarFantasma();
+        f1.Mover("Direita");
+    }
+}
