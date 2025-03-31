2f8852e8f2160658b5e2f598c632fb508b309841
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-03-31 02:02:48 -0300

    Roteiro 2 exercicio 1
    Roteiro 2 exercicio  1 - Produto e Preço

---
+++ Roteiro2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+public class Produto {
+    
+    private string  Nome {get; set;}
+    private double Preco;
+
+    public Produto(string nome, double preco) {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public double preco {
+        get {return Preco;}
+
+        set {
+            if (Preco < 0) {
+                Console.WriteLine("Preco invalido");
+            }
+        }
+    }
+
+    public void ExibirDetalhes() {
+        Console.WriteLine($"O PRODUTO E {Nome} E O PRECO {Preco}");
+    }
+
+}
+
+public class Program {
+    public static void Main(){
+        Produto produto1 = new Produto("Celular", 1500);
+        produto1.ExibirDetalhes();
+        produto1.Preco = -200;
+    }
+}
