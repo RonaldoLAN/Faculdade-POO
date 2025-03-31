d50355535e7dfcf269311764818a0c4b25a05cba
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-03-31 03:21:20 -0300

    Roteiro 3 exercicio 1
    Roteiro 3 exercicio 1 - Subclasse Animal

---
+++ Roteiro3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Animal {
+    public string Nome;
+
+    public Animal(string nome) {
+        Nome = nome;
+    }
+
+    public void EmitirSom() {
+        Console.WriteLine("Som generico de animal!");
+    }
+}
+
+public class Cachorro : Animal {
+
+    public Cachorro(string nome) : base(nome) { }
+    
+    public void EmitirSom() {
+        Console.WriteLine("AU AU AU!");
+    }
+}
+
+public class Program {
+    public static void Main() {
+        Cachorro Bella = new Cachorro("Bella");
+        Bella.EmitirSom();
+    }
+}
+
