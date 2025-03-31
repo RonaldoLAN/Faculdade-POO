630264d0bf8b5348e7f5fe1b3c00f55fb7dcb259
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-03-31 03:38:40 -0300

    Roteiro 3 exercicio 2
    Roteiro 3 exercicio 2 - Pedido/Item

---
+++ Roteiro3-ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Item {
+
+    public string Nome;
+    public int Valor;
+
+    public void MostrarItem() {
+        Console.WriteLine($"Seu produto e {Nome} e o valor {Valor}");
+    }
+
+
+}
+
+public class Pedido {
+
+    private Item item;
+
+    public Pedido() {
+        item = new Item();
+    }
+
+    public void MostrarPedido() {
+        Console.WriteLine("Seu pedido foi feito");
+        item.MostrarItem();
+    }
+}
+
+
+public class Program {
+    public static void Main() {
+        Pedido pedido = new Pedido();
+        pedido.MostrarPedido();
+    }
+}
