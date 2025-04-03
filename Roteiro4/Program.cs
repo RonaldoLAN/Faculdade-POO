52825a96bcc5254077817dcc8d0e12859720c32f
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-04-02 23:47:49 -0300

    Roteiro 4 Exercicio 1
    Roteiro 4 Exercicio 1 - Pagamento

---
+++ Roteiro4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Pagamento {
+    public virtual void ProcessarPagamento() {
+        Console.WriteLine("Pagamento");
+    }
+}
+
+class CartaoCredito : Pagamento {
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Pagamento feito com Cartao de Credito");
+    }
+}
+
+class BoletoBancario : Pagamento {
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Pagamento feito com Boleto bancario");
+    }
+}
+
+class Pix : Pagamento {
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Pagamento feito com Pix");
+    }
+}
+
+public class Program {
+    public static void Main() {
+        List<Pagamento> pagamentos = new List<Pagamento>
+        {
+            new CartaoCredito(),
+            new BoletoBancario(),
+            new Pix()
+        };
+
+        foreach (var pagamento in pagamentos)
+        {
+            pagamento.ProcessarPagamento();
+        }
+    }
+}
