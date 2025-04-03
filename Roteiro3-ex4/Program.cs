e995272723dfe1a0078f627a38ab38ec430a17fe
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-04-02 23:20:56 -0300

    Roteiro 3 exercicio 4
    Roteiro 3 exercicio 4 - Pato, Aguia e peixe

---
+++ Roteiro3-ex4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public interface IAnimal {
+    void Voar();
+    void Nadar();
+}
+
+public class Pato : IAnimal {
+    public void Voar() {
+        Console.WriteLine("Pato esta voando!");
+    }
+    public void Nadar() {
+        Console.WriteLine("Pato esta nadando!");
+    }
+}
+
+public class Aguia : IAnimal {
+    public void Voar() {
+        Console.WriteLine("Aguia esta voando!");
+    }
+    public void Nadar() {
+        Console.WriteLine("Aguia nao pode nadar!");
+    }
+}
+
+public class Peixe : IAnimal {
+    public void Voar() {
+        Console.WriteLine("Peixe nao pode voar!");
+    }
+    public void Nadar() {
+        Console.WriteLine("Peixe esta nadando!");
+    }
+}
+
+public class Program {
+    public static void Main() {
+        IAnimal pato = new Pato();
+        IAnimal aguia = new Aguia();
+        IAnimal peixe = new Peixe();
+
+        pato.Nadar();
+        pato.Voar();
+
+        aguia.Nadar();
+        aguia.Voar();
+
+        peixe.Nadar();
+        peixe.Voar();
+    }
+}
