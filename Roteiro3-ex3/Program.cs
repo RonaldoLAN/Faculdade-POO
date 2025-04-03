1093f510ce384a6038fd26ca0ec9bd8fc597be7a
Ronaldo Luiz <143454938+RonaldoLAN@users.noreply.github.com>
2025-04-02 23:06:41 -0300

    Roteiro 3 exercicio 3
    Roteiro 3 exercicio 3 - Carro e bicicleta

---
+++ Roteiro3-ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public interface IVeiculo {
+    void Mover();
+}
+
+public class Carro : IVeiculo {
+    public void Mover() {
+        Console.WriteLine("Carro esta se movendo");
+    }
+}
+
+public class Bicicleta : IVeiculo {
+    public void Mover() {
+        Console.WriteLine("Bicicleta esta se movendo");
+    }
+}
+
+public class Program {
+    public static void Main() {
+        IVeiculo carro1 = new Carro();
+        IVeiculo bicicleta1 = new Bicicleta();
+
+        carro1.Mover();
+        bicicleta1.Mover();
+    }
+}
